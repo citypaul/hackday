a917cc8198bfc8a0113db2bc6e10b24e708b1afd
Marina Shchukina <shchukina.marina@gmail.com>
2016-09-29 16:33:06 +0100

    Adding graph (commented out)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 /* eslint-disable */
 import React, { Component } from 'react';
+import {Line} from 'react-chartjs';
+//var LineChart = require("react-chartjs").Line;
+import logo from './logo.svg';
+import './App.css';
 import PercentageBar from './percentage-bar';
 import { merge, reduce, map, pick } from 'lodash';
 
@@ -199,6 +203,41 @@ const App = React.createClass({
     },
 
     render() {
+        const chartData =  {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+        };
+        const chartOptions = {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        };
+        //return <Line data={chartData} options={chartOptions} width="600" height="250"/>
+
         return (
             <div>
                 <div>
@@ -217,9 +256,10 @@ const App = React.createClass({
                     </thead>
                     {this.generateTableBody()}
                 </table>
-                
+
             </div>
         );
+    //}
     }
 });
 

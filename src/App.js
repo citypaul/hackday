80a45ea18d05fa21876ba0e7e76ef3ed6c94b719
Paul Hammond <paul.hammond@bbc.co.uk>
2016-09-29 14:01:35 +0100

    updated with stats we think are relevant from the f24

--- src/App.js
+++ src/App.js
@@ -5,46 +5,52 @@ import PercentageBar from './percentage-bar';
 import { merge, reduce } from 'lodash';
 
 const App = React.createClass({
+    getInitialTeamModel() {
+        return {
+            goals: 0,
+            possession: 50,
+            shotsOnTarget: 0,
+            shotsBlocked: 0,
+            shotsOffTarget: 0,
+            fouls: 0,
+            yellowCards: 0,
+            redCards: 0,
+            penalties: 0,
+            corners: 0,
+            crosses: 0,
+            turnOvers: 0,
+            //passCompletions: 0,
+            nonAttackingFreekicks: 0,
+            attackingFreekicks: 0,
+            attackingPasses: 0,
+            bigChances: 0,
+            divingSaves: 0
+        }
+    },
+
     getInitialState() {
         return {
-            home: {
-                goals: 0,
-                possession: 50,
-                shotsOnTarget: 0,
-                shotsOffTarget: 0,
-                fouls: 0,
-                redCards: 0,
-                penalties: 0,
-                corners: 0,
-                crosses: 0,
-                //passCompletions: 0,
-                freeKicks: 0
-            },
-            away: {
-                goals: 0,
-                possession: 50,
-                shotsOnTarget: 0,
-                shotsOffTarget: 0,
-                fouls: 0,
-                redCards: 0,
-                penalties: 0,
-                corners: 0,
-                crosses: 0,
-                //passCompletions: 0,
-                freeKicks: 0
-            },
+            home: this.getInitialTeamModel(),
+            away: this.getInitialTeamModel(),
             weights: {
                 goals: 10,
                 possession: 1,
                 shotsOnTarget: 4,
+                shotsBlocked: 1,
                 shotsOffTarget: 2,
                 fouls: -1, // should we get rid of those? we already have freeKicks
                 redCards: -10, // yellow card?
+                yellowCards: -5,
                 penalties: 3,
                 corners: 2,
                 crosses: 1,
+                turnOvers: -5,
+                attackingFreekicks: 2,
                 //passCompletions: 1, percentage, like possession, but cumulative
-                freeKicks: 1
+                nonAttackingFreekicks: 1,
+                attackingPasses: 1,
+                bigChances: 10,
+                divingSaves: -4
             },
             points: {
                 home: 0,
@@ -145,12 +151,19 @@ const App = React.createClass({
                 {this.generateTableRow('goals')}
                 {this.generateTableRow('shotsOnTarget')}
                 {this.generateTableRow('shotsOffTarget')}
+                {this.generateTableRow('shotsBlocked')}
                 {this.generateTableRow('possession')}
                 {this.generateTableRow('fouls')}
                 {this.generateTableRow('redCards')}
+                {this.generateTableRow('yellowCards')}
                 {this.generateTableRow('penalties')}
                 {this.generateTableRow('crosses')}
-                {this.generateTableRow('freeKicks')}
+                {this.generateTableRow('nonAttackingFreekicks')}
+                {this.generateTableRow('attackingFreekicks')}
+                {this.generateTableRow('turnOvers')}
+                {this.generateTableRow('attackingPasses')}
+                {this.generateTableRow('bigChances')}
+                {this.generateTableRow('divingSaves')}
             </tbody>
             );
     },

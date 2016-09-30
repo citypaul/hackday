3daa6e5ff24d7c7a22f88de1d367cfd97c5e5909
Marina Shchukina <shchukina.marina@gmail.com>
2016-09-30 10:04:23 +0100

    Added match view container.

---
+++ src/containers/match-view-container.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import SnapshotList from './../components/snapshot-list';
+import jquery from 'jquery';
+
+const MatchViewContainer = React.createClass({
+        render() {
+        const style = { color: 'peru' };
+
+        return (
+            <h1>Hello, match view container!</h1>
+        );
+    }
+});
+
+
+export default MatchViewContainer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SnapshotGeneratorContainer from './containers/snapshot-generator-container';
+import MatchViewContainer from './containers/match-view-container';
 import './main.css';
 import { Router, Route, Link, browserHistory } from 'react-router'
 
@@ -8,7 +9,11 @@ import { Router, Route, Link, browserHistory } from 'react-router'
 ReactDOM.render(
     <Router history={browserHistory}>
       <Route path="/" component={SnapshotGeneratorContainer}>
-        <Route path="match" component={SnapshotGeneratorContainer}/>
+        <Route path="/" component={SnapshotGeneratorContainer}/>
+      </Route>
+
+      <Route path="/match" component={MatchViewContainer}>
+        <Route path="match" component={MatchViewContainer} />
       </Route>
     </Router>,
   document.getElementById('root')

68a343a5fd8aeb4dc496a5aa7cbe6d31add69f12
Paul Hammond <paul.hammond@bbc.co.uk>
2016-09-30 09:46:45 +0100

    react router

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SnapshotGeneratorContainer from './containers/snapshot-generator-container';
 import './main.css';
+import { Router, Route, Link, browserHistory } from 'react-router'
+
 
 ReactDOM.render(
-  <SnapshotGeneratorContainer />,
+    <Router history={browserHistory}>
+      <Route path="/" component={SnapshotGeneratorContainer}>
+        <Route path="match" component={SnapshotGeneratorContainer}/>
+      </Route>
+    </Router>,
   document.getElementById('root')
 );

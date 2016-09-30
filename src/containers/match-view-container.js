e4a62f10a18b48a191e2f27b6b9df4aa288620e1
Paul Hammond <paul.hammond@bbc.co.uk>
2016-09-30 10:31:11 +0100

    making requests on the page

--- src/containers/match-view-container.js
+++ src/containers/match-view-container.js
@@ -4,7 +4,31 @@ import PercentageBar from '../components/percentage-bar';
 import jquery from 'jquery';
 
 const MatchViewContainer = React.createClass({
-    
+    getInitialState() {
+        return {
+            scenarioIndex: 0
+        }
+    },
+
+    requestData() {
+        let scenarioUrl = 'http://localhost:3001/scenarios/' + this.props.params.scenario + '/' + this.state.scenarioIndex;
+        jquery.ajax(scenarioUrl, {
+            success: function (data) {
+                let newIndex = this.state.scenarioIndex + 1;
+                this.setState({
+                    scenarioIndex: newIndex
+                })
+            }.bind(this),
+            error: function () {
+
+            }
+        });
+    },
+
+    componentDidMount() {
+        setInterval(this.requestData, 2000);
+    },
+
     render() {
         console.log(this.props);
         const style = {color: 'peru'};
